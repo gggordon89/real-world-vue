2733979807d890457bf261e179acaa35b1174228
Greg Gordon <gggordon89@gmail.com>
2020-06-03 13:56:44 +0200

    Success & Error Notifications

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,19 @@
   <div id="app">
     <div id="nav">
       <NavBar />
-      <router-view :key="$route.fullPath"/>
+      <NotificationContainer />
+      <router-view :key="$route.fullPath" />
     </div>
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
-
+import NotificationContainer from '@/components/NotificationContainer.vue'
 export default {
   components: {
-    NavBar
+    NavBar,
+    NotificationContainer
   }
 }
 </script>
